dca5a1c3096ddcee4b724c97b8b34a50bebd7ef8
JoaoVSN <jvieira850@gmail.com>
2025-07-19 20:28:53 -0300

    feat: colocando um timer

--- src/main/java/com/kafka/kafkatest/application/service/KafkaServiceImpl.java
+++ src/main/java/com/kafka/kafkatest/application/service/KafkaServiceImpl.java
@@ -6,12 +6,14 @@ import com.kafka.kafkatest.application.KafkaService;
 import com.kafka.kafkatest.application.domain.Order;
 import com.kafka.kafkatest.application.mapper.TestMapper;
 import com.kafka.kafkatest.application.port.MessageAdapter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class KafkaServiceImpl implements KafkaService {
     @Autowired
@@ -25,10 +27,15 @@ public class KafkaServiceImpl implements KafkaService {
 
     @Override
     public OrderResponse producerKafkaMsg(ClientRequest body) {
-        Order newOrder =  TestMapper.INSTANCE.clientResquestToOrder(UUID.randomUUID().toString(),body);
-
-        return TestMapper.INSTANCE.orderToOrderResponse(
-            messageAdapter.produceMessage(newOrder)
-        );
+        try{
+            Order newOrder =  TestMapper.INSTANCE.clientResquestToOrder(UUID.randomUUID().toString(),body);
+            Thread.sleep(2000);
+            return TestMapper.INSTANCE.orderToOrderResponse(
+                messageAdapter.produceMessage(newOrder)
+            );
+        }catch (InterruptedException exception){
+            log.error("Deu problema no contador!!!", exception);
+            throw new RuntimeException();
+        }
     }
 }
